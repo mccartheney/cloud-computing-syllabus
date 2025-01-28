c636c558036f66be725d31bc7fb0e4babfba720a
mccartheney mendes <mccartheney@hotmail.com>
2025-01-28 22:00:24 +0000

    FEAT: create cluster by client name

--- TerraformProject/main.tf
+++ TerraformProject/main.tf
@@ -2,9 +2,39 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
+
+
 resource "kubernetes_namespace" "client_namespace" {
   metadata {
     name = "${var.client}-${var.environment}"
+  }
+}
+
+
+variable "minikube_driver" {
+  default = "docker"
+}
+
+variable "minikube_cpus" {
+  default = 2
+}
+
+variable "minikube_memory" {
+  default = 4096
+}
+
+resource "null_resource" "minikube_profile" {
+  provisioner "local-exec" {
+    command = <<EOT
+    if ! minikube profile list | grep -q "${var.client}"; then
+      minikube start --profile=${var.client} \
+        --driver=${var.minikube_driver} \
+        --cpus=${var.minikube_cpus} \
+        --memory=${var.minikube_memory}
+    else
+      echo "Minikube profile '${var.client}' already exists."
+    fi
+    EOT
   }
 }
 
@@ -29,3 +59,4 @@ module "ingress" {
   domain = var.domain
   namespace  = kubernetes_namespace.client_namespace.metadata[0].name
 }
+
